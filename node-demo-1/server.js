02ee447c3f03578d9340e3fea97b78bac1cb0fd8
82XfishBo <18727302521@163.com>
2022-08-10 11:32:01 +0800

    无事发生

--- node-demo-1/server.js
+++ node-demo-1/server.js
@@ -29,23 +29,32 @@ var server = http.createServer(function(request, response){
         <html lang="zh-CN">
         <head>
               <meta charset="UTF-8">
-              <link rel="stylesheet" href="/x">
+              <link rel="stylesheet" href="/css">
+              <title>AJAX</title>
               </head>
 
               <body>
                    <h1>大清已经亡啦，陛下！</h1>
+                   <h3>现在是AJAX的天下</h3>
+                   <script src="/main.js"></script>
               </body>
-
         </html>
         `)
         response.end()
     }
-   else if(path === '/x'){
+   else if(path === '/css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`h1{font-size:30px;color: red;}`)
+      response.write(`h1
+    {font-size:30px;color: red;}`)
     response.end()
-      } 
+    }
+    else if(path === '/main.js'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+      response.write(`console.log("hi")`)
+    response.end()
+      }
     else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
